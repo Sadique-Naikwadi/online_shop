a5fb8c8db838b98ba70edffa7be22f63dcc1d9bc
Sadique Naikwadi <sadique.techie@gmail.com>
2024-01-10 19:17:22 +0530

    Add product_details page and products_by_category functionality to the project.

--- shop/urls.py
+++ shop/urls.py
@@ -5,4 +5,6 @@ app_name ='shop'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<slug:category_slug>/', views.index, name='products_by_category'),
+    path('<str:pk>/<slug:slug>/',views.product_details, name='product_details'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,9 +1,18 @@
-from django.shortcuts import render
-from .models import Product
+from django.shortcuts import render, get_object_or_404
+from .models import Product, Category
 
 # Create your views here.
 
-def index(request):
-    products = Product.objects.filter(available=True)
+def index(request, category_slug=None):
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        products = Product.objects.filter(category=category,available=True)
+    else:
+        products = Product.objects.filter(available=True)
     context = {'products': products}
     return render(request, 'shop/index.html', context)
+
+def product_details(request,pk,slug):
+    product = get_object_or_404(Product, id=pk, slug=slug)
+    context = {'product': product}
+    return render(request, 'shop/product_details.html', context)
