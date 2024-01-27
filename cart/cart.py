8924821faa3f26a59a031d6c08038837bf8f6faf
Sadique Naikwadi <sadique.techie@gmail.com>
2024-01-27 20:46:33 +0530

    Creates Cart Class in the project.

---
+++ cart/cart.py
@@ -0,0 +1,76 @@
+from django.conf import settings
+from shop.models import Product
+from decimal import Decimal
+
+class Cart:
+    
+    def __init__(self, request):
+        self.session = request.session
+
+        self.cart = self.session.get(settings.CART_SESSION_ID)
+        if not self.cart:
+            self.cart = {}
+            self.session[settings.CART_SESSION_ID] = self.cart
+
+    def add(self, product, quantity=1):
+
+        product_id = str(product.id)
+        if product_id not in self.cart:
+            self.cart[product_id] = {'quantity': quantity, 'price': str(product.price)}
+
+        else:
+            self.cart[product_id]['quantity'] = quantity
+
+        
+        self.save()
+
+    
+    def save(self):
+        self.session.modified = True
+    
+
+    def remove(self, product):
+        product_id = str(product.id)
+
+        if product_id in self.cart:
+            del self.cart[product_id]
+            self.save()
+        
+    
+    def __iter__(self):
+        product_ids = self.cart.keys()
+        products = Product.objects.filter(id__in=product_ids)
+
+        for product in products:
+            self.cart[str(product.id)]['product'] = product
+        
+        cart = self.cart.copy()
+
+        for item in cart.values():
+            item['price'] = Decimal(item['price'])
+            item['total_price'] = item['price'] * item['quantity']
+            yield item
+        
+    
+    def __len__(self):
+        total_quantity = 0
+        for item in self.cart.values():
+            total_quantity += item['quantity']
+
+        return total_quantity
+    
+    def get_total_price(self):
+        total_price = 0
+        for item in self.cart.values():
+            total_price += item['quantity'] * Decimal(item['price'])
+        
+        return total_price
+
+    def clear(self):
+        del self.session[settings.CART_SESSION_ID]
+        self.save()
+    
+    
+    
+        
+    
